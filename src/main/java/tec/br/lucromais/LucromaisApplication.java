b6f369fb7b863141965dc88bf554fd10687e9dd7
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-02-23 10:01:15 -0300

    Repositório inicial

---
+++ src/main/java/tec/br/lucromais/LucromaisApplication.java
@@ -0,0 +1,17 @@
+package tec.br.lucromais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe principal da aplicação lucromais
+ * @author Thiago Pinheiro do Nascimento
+ * @version 0.1
+ * @since 0.1
+**/
+@SpringBootApplication
+public class LucromaisApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LucromaisApplication.class, args);
+	}
+}
---
+++ src/test/java/tec/br/lucromais/LucromaisApplicationTests.java
@@ -0,0 +1,13 @@
+package tec.br.lucromais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucromaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
