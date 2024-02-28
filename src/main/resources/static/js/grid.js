8aa5ef0cb41e80fb7b1a3f42fdf227e5d71a1ec3
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-02-28 16:03:30 -0300

    Arquivos estáticos do template

---
+++ src/main/resources/static/js/grid.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".grid").DataTable({
+    "responsive": true,
+    "language": {
+        "url": "https://cdn.datatables.net/plug-ins/1.13.7/i18n/pt-BR.json",
+    }
+  });
+});
