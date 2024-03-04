81db368645179f30fdd467ea2068e14cc4a685eb
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-03-04 15:22:49 -0300

    Atualização dos inserts de usuários iniciais para compor perfis de sistema, admin e cliente

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,10 +1,11 @@
-INSERT INTO roles (name) VALUES ("ADMINISTRADOR")
-INSERT INTO roles (name) VALUES ("CLIENTE")
+INSERT INTO roles (authority) VALUES ("SISTEMA")
+INSERT INTO roles (authority) VALUES ("ADMINISTRADOR")
+INSERT INTO roles (authority) VALUES ("CLIENTE")
 
 INSERT INTO users (username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled) VALUES ("02605077330", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, 1, 1, 1)
 INSERT INTO users (username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled) VALUES ("97787817291", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, 1, 1, 1)
 INSERT INTO users (username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled) VALUES ("03234475385", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, 1, 1, 1)
 
 INSERT INTO authorities VALUES (1, 1)
-INSERT INTO authorities VALUES (1, 2)
-INSERT INTO authorities VALUES (2, 3)
+INSERT INTO authorities VALUES (2, 2)
+INSERT INTO authorities VALUES (3, 3)
