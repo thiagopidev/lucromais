5796808aeba09a9914a9dfa8896287f105a77a23
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-03-04 11:09:37 -0300

    Interface de repositório de grupos de usuários

---
+++ src/main/java/tec/br/lucromais/repositories/RoleRepository.java
@@ -0,0 +1,17 @@
+package tec.br.lucromais.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import tec.br.lucromais.models.Role;
+
+/**
+ * Interface de repositório de grupos de usuários
+ * @author Thiago Pinheiro do Nascimento
+ * @version 0.1
+ * @since 0.1
+**/
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
