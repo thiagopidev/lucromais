79d7f3cdbcd7c039bbd1f505428dd402e77913ee
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-03-07 16:57:33 -0300

    Atualização do insert de autorizações de usuários, acrescentando created_at e updated_at

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
-INSERT INTO roles (authority) VALUES ("SISTEMA")
-INSERT INTO roles (authority) VALUES ("ADMINISTRADOR")
-INSERT INTO roles (authority) VALUES ("CLIENTE")
+INSERT INTO roles (authority, created_at, updated_at) VALUES ("SISTEMA", utc_timestamp, utc_timestamp)
+INSERT INTO roles (authority, created_at, updated_at) VALUES ("ADMINISTRADOR", utc_timestamp, utc_timestamp)
+INSERT INTO roles (authority, created_at, updated_at) VALUES ("CLIENTE", utc_timestamp, utc_timestamp)
 
 INSERT INTO users (username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled) VALUES ("02605077330", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, 1, 1, 1)
 INSERT INTO users (username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled) VALUES ("97787817291", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, 1, 1, 1)
