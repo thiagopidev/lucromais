1a7922389e1b2026779f0a18c397cc0f6f80c5e0
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-03-11 16:35:33 -0300

    Atualização da importação de usuários iniciais, acrescentando usualname e email

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,9 +2,9 @@ INSERT INTO roles (authority, created_at, updated_at) VALUES ("SISTEMA", utc_tim
 INSERT INTO roles (authority, created_at, updated_at) VALUES ("ADMINISTRADOR", utc_timestamp, utc_timestamp)
 INSERT INTO roles (authority, created_at, updated_at) VALUES ("CLIENTE", utc_timestamp, utc_timestamp)
 
-INSERT INTO users (username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, created_at, updated_at) VALUES ("02605077330", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, 1, 1, 1, utc_timestamp, utc_timestamp)
-INSERT INTO users (username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, created_at, updated_at) VALUES ("97787817291", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, 1, 1, 1, utc_timestamp, utc_timestamp)
-INSERT INTO users (username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, created_at, updated_at) VALUES ("03234475385", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, 1, 1, 1, utc_timestamp, utc_timestamp)
+INSERT INTO users (username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, created_at, updated_at, usualname, email) VALUES ("02605077330", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, 1, 1, 1, utc_timestamp, utc_timestamp, "Thiago Pinheiro", "thiago@gmail.com")
+INSERT INTO users (username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, created_at, updated_at, usualname, email) VALUES ("97787817291", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, 1, 1, 1, utc_timestamp, utc_timestamp, "Júnior Fernandes", "jr@gmail.com")
+INSERT INTO users (username, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, created_at, updated_at, usualname, email) VALUES ("03234475385", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, 1, 1, 1, utc_timestamp, utc_timestamp, "Susy Barros", "su@gmail.com")
 
 INSERT INTO authorities VALUES (1, 1)
 INSERT INTO authorities VALUES (2, 2)
