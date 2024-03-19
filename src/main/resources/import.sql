0935e7abcfd96710ef844f10d0cd3dadac7b8b95
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-03-19 10:11:53 -0300

    Adequação do arquivo de importação à mudança de usuário e perfil

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,11 +1,3 @@
-INSERT INTO roles (authority, created_at, updated_at) VALUES ("Sistema", utc_timestamp, utc_timestamp)
-INSERT INTO roles (authority, created_at, updated_at) VALUES ("Administrador", utc_timestamp, utc_timestamp)
-INSERT INTO roles (authority, created_at, updated_at) VALUES ("Cliente", utc_timestamp, utc_timestamp)
-
-INSERT INTO users (username, password, enabled, created_at, updated_at, usualname, email) VALUES ("02605077330", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, utc_timestamp, utc_timestamp, "Thiago Pinheiro", "thiago@gmail.com")
-INSERT INTO users (username, password, enabled, created_at, updated_at, usualname, email) VALUES ("97787817291", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, utc_timestamp, utc_timestamp, "Júnior Fernandes", "jr@gmail.com")
-INSERT INTO users (username, password, enabled, created_at, updated_at, usualname, email) VALUES ("03234475385", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", 1, utc_timestamp, utc_timestamp, "Susy Barros", "su@gmail.com")
-
-INSERT INTO authorities VALUES (1, 1)
-INSERT INTO authorities VALUES (2, 2)
-INSERT INTO authorities VALUES (3, 3)
+INSERT INTO users (cpf, name, email, password, role, created_at, updated_at) VALUES ("02605077330", "Thiago Pinheiro", "thiago@gmail.com", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", "SYS", utc_timestamp, utc_timestamp)
+INSERT INTO users (cpf, name, email, password, role, created_at, updated_at) VALUES ("97787817291", "Júnior Fernandes", "jr@gmail.com", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", "ADM", utc_timestamp, utc_timestamp)
+INSERT INTO users (cpf, name, email, password, role, created_at, updated_at) VALUES ("03234475385", "Susy Barros", "susy@gmail.com", "$2a$10$0tvcBCgR032yw7AlIrSgt.RKgu8wHRRYF.uOC.hSRuYm8mzVcWb6a", "CLI", utc_timestamp, utc_timestamp)
