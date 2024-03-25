5d43106e6f422f7e9eef3f1366f2910ea0ef6bae
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-03-25 17:23:31 -0300

    Correção de bug da máscara dinâmica de cpf e cnpj

--- src/main/resources/static/js/client.type.js
+++ src/main/resources/static/js/client.type.js
@@ -10,13 +10,21 @@ ClientType.DinamicMask = (function() {
 	
 	DinamicMask.prototype.start = function() {
 		this.radio.on('change', onSelectedClientType.bind(this));
+		var selectedType = this.radio.filter(':checked')[0];
+		if (selectedType) {
+			applyMask.call(this, $(selectedType));
+		}
 	}
 	
 	function onSelectedClientType(event) {
 		var selectedClientType = $(event.currentTarget);		
+		applyMask.call(this, $(selectedClientType));
+		this.input.val('');
+	}
+	
+	function applyMask(selectedClientType) {
 		this.label.text(selectedClientType.data('document'));
 		this.input.mask(selectedClientType.data('mask'));
-		this.input.val('');
 		this.input.removeAttr('disabled');
 	}
 	return DinamicMask;
