01c1f060ff8a1cf3eabce7eb123ada52946dffbc
Thiago Pinheiro <thiagopinheiro.developer@gmail.com>
2024-03-25 17:43:19 -0300

    Interface de grupo de validação de cpf

---
+++ src/main/java/tec/br/lucromais/groups/CpfGroup.java
@@ -0,0 +1,11 @@
+package tec.br.lucromais.groups;
+
+/**
+ * Interface de grupo de validação de cpf
+ * @author Thiago Pinheiro do Nascimento
+ * @version 0.1
+ * @since 0.1
+**/
+public interface CpfGroup {
+
+}
